043ecd5634392776978cd8e96d8e6ac5cd69ef40
Bogdan Markov <bogdick@yandex.ru>
2018-03-01 23:16:13 +0300

    Logging is added in HttpbinLibrary, 'assert' is replaced with 'if ... raise AssertionError(...)', docstrings are changed.

--- HttpbinTestSuit/HttpbinLibrary.py
+++ HttpbinTestSuit/HttpbinLibrary.py
@@ -1,7 +1,7 @@
-# todo: add logging
+# todo: add tags
 
 # todo? rename module to httpbin_library.py
-# todo? robot framework documentation
+# todo? add documentation to robot framework files
 
 # todo: name columns where applicable
 """Robot Framework library for testing http://httpbin.org/.
@@ -14,6 +14,8 @@ Endpoints to test:
 
 import requests
 from robot.api.deco import keyword
+from robot.api import logger
+
 
 class HttpbinLibrary:
     """Main (and the only) class of the library."""
@@ -23,6 +25,16 @@ class HttpbinLibrary:
     def __init__(self):
         """Initialize self."""
         self._response = None
+
+    def _log_response(self):
+        if self._response is not None:
+            logger.info('Status code: ' + str(self._response.status_code))
+            logger.debug('Headers:\n' + str(self._response.headers))
+            logger.info('Response body:\n' + self._response.text)
+            logger.debug('Hexadecimal response body:\n'
+                         + self._response.content.hex())
+        else:
+            logger.warn('Response is None')
 
     @keyword('Authenticate ${correct_user}/${correct_password}'
              ' as ${entered_user}/${entered_password}')
@@ -44,8 +56,6 @@ class HttpbinLibrary:
         :type entered_password: str
         :rtype: None
         """
-        print(correct_user, correct_password, sep='::')
-        print(entered_user, entered_password, sep='::')
         self._response = requests.get(
             self._BASE_URL + 'basic-auth/{}/{}'.format(
                 requests.utils.quote(correct_user),
@@ -53,6 +63,7 @@ class HttpbinLibrary:
             ),
             auth=(entered_user, entered_password),
         )
+        self._log_response()
 
     @keyword('Send GET request with headers')
     def get(self, headers=None):
@@ -63,6 +74,7 @@ class HttpbinLibrary:
         :rtype: None
         """
         self._response = requests.get(self._BASE_URL + 'get', headers=headers)
+        self._log_response()
 
     @keyword('Get stream of ${n} lines')
     def stream(self, n):
@@ -74,81 +86,111 @@ class HttpbinLibrary:
         :rtype: None
         """
         self._response = requests.get(self._BASE_URL + 'stream/{}'.format(n))
+        self._log_response()
 
     def status_code_should_be(self, expected_status_code):
         """Check if response status code is as expected.
 
         Should invoke one of the following methods first:
-            basic_auth,
-            get,
-            stream.
+            HttpbinLibrary.basic_auth,
+            HttpbinLibrary.get,
+            HttpbinLibrary.stream.
 
         :type expected_status_code: int
         :rtype: None
         """
-        assert str(self._response.status_code) == str(expected_status_code), \
-            'Status code should be {} but was {}.'.format(
-                expected_status_code,
-                self._response.status_code,
+        if str(self._response.status_code) != str(expected_status_code):
+            raise AssertionError(
+                "Status code should be '{}', but '{}' received.".format(
+                    expected_status_code,
+                    self._response.status_code,
+                )
             )
 
     @keyword('User ${username} should be authenticated')
     def check_authentication(self, username):
         """Check if user is authenticated with correct username.
 
-        Should invoke method basic_auth first.
+        Should invoke HttpbinLibrary.basic_auth first.
 
         :param username: correct authentication username
         :type username: int
         :rtype: None
         """
-        assert self._response.json()['authenticated'] is True, \
-            'User is not authenticated'
-        assert self._response.json()['user'] == username, \
-            'Username should be {} but was {}'.format(
-                username,
-                self._response.json()['user'],
+        json_response = self._response.json()
+        if 'authenticated' not in json_response:
+            raise AssertionError(
+                "Response body does not contain 'authenticated' key.")
+        if json_response['authenticated'] is not True:
+            raise AssertionError('User is not authenticated.')
+        if 'user' not in json_response:
+            raise AssertionError("Response body does not contain 'user' key.")
+        if json_response['user'] != username:
+            raise AssertionError(
+                "Username should be '{}', but '{}' received.".format(
+                    username,
+                    json_response['user'],
+                )
             )
 
     def response_body_should_contain_headers(self, headers):
         """Check if response body contain specified headers.
 
-        Should invoke method get first.
+        Should invoke HttpbinLibrary.get first.
 
         :param headers: headers to be in response body
         :type headers: Dict[str, str]
         :rtype: None
         """
-        # print(headers.items())
-        # print(self._response.text)
+        json_response = self._response.json()
+        if 'headers' not in json_response:
+            raise AssertionError(
+                "Response body does not contain 'headers' key.")
         for name, value in headers.items():
-            # print(name, value)
-            assert self._response.json()['headers'][name.title()] == value
+            titled = name.title()
+            if titled not in json_response['headers']:
+                raise AssertionError(
+                    "Response body does not contain '{}' header.".format(
+                        titled)
+                )
+            if json_response['headers'][titled] != value:
+                raise AssertionError(
+                    "Header '{}' should contain '{}', but '{}'"
+                    " received.".format(titled, value, json_response[titled],)
+                )
 
     def response_body_should_not_contain_headers(self, headers_names):
         """Check if response body not contain specified headers.
 
-        Should invoke method get first.
+        Should invoke HttpbinLibrary.get first.
 
         :param headers_names: headers to not be in response body
         :type headers_names: List[str]
         :rtype: None
         """
-        print(headers_names)
+        json_response = self._response.json()
+        if 'headers' not in json_response:
+            raise AssertionError(
+                "Response body does not contain 'headers' key.")
         for name in headers_names:
-            assert name.title() not in self._response.json()['headers']
+            titled = name.title()
+            if titled in json_response['headers']:
+                raise AssertionError("Response body contains '{}' header,"
+                                     " but should not.".format(titled))
 
     def number_of_lines_should_be(self, expected_number_of_lines):
         """Check if number of lines in response body is as expected.
 
-        Should invoke method stream first.
+        Should invoke HttpbinLibrary.stream first.
 
         :type expected_number_of_lines: int
         :rtype: None
         """
         actual_number_of_lines = self._response.text.count('\n')
-        assert actual_number_of_lines == int(expected_number_of_lines), \
-            'Number of lines should be {} but was {}'.format(
-                expected_number_of_lines,
-                actual_number_of_lines,
+        if actual_number_of_lines != int(expected_number_of_lines):
+            raise AssertionError(
+                "Number of lines should be '{}', but '{}' received.".format(
+                    expected_number_of_lines,
+                    actual_number_of_lines,
+                )
             )
